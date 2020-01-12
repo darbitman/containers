dbb22b01bc534e4148d4a0c1a4c99add13241f2f
Dmitriy A <d.arbitman@gmail.com>
2020-01-12 15:30:28 -0800

    Fixed bench

--- src/bench/SharedQueueBench.cpp
+++ src/bench/SharedQueueBench.cpp
@@ -2,49 +2,67 @@
 
 #include <atomic>
 #include <cstdint>
+#include <iostream>
 
 #include "SharedQueue.hpp"
 
-static void BM_PushSharedQueueBlocking(benchmark::State& state) {
-    ctr::SharedQueue<uint32_t> queue;
+//------------------------------------------------------------------------------
 
-    uint32_t max = state.range(0);
+static void BM_PushSharedQueueBlocking(benchmark::State& state) {
+    // how many values to push to the queue
+    uint32_t num_values = state.range(0);
 
     for (auto _ : state) {
-        for (uint32_t i = 0; i < max; ++i) {
+        ctr::SharedQueue<uint32_t, true> queue;
+
+        for (uint32_t i = 0; i < num_values; ++i) {
             queue.push(i);
         }
+
+        benchmark::DoNotOptimize(queue);
     }
 }
 
 BENCHMARK(BM_PushSharedQueueBlocking)->RangeMultiplier(10)->Range(100, 1000000);
 
-static void BM_PushSharedQueueNonblocking(benchmark::State& state) {
-    ctr::SharedQueue<uint32_t, false> queue;
+//------------------------------------------------------------------------------
 
-    uint32_t max = state.range(0);
+static void BM_PushSharedQueueNonblocking(benchmark::State& state) {
+    // how many values to push to the queue
+    uint32_t num_values = state.range(0);
 
     for (auto _ : state) {
-        for (uint32_t i = 0; i < max; ++i) {
+        ctr::SharedQueue<uint32_t, false> queue;
+
+        for (uint32_t i = 0; i < num_values; ++i) {
             queue.push(i);
         }
+
+        benchmark::DoNotOptimize(queue);
     }
 }
 
 BENCHMARK(BM_PushSharedQueueNonblocking)->RangeMultiplier(10)->Range(100, 1000000);
 
-static void BM_PushStdQueue(benchmark::State& state) {
-    std::queue<uint32_t> queue;
+//------------------------------------------------------------------------------
 
-    uint32_t max = state.range(0);
+static void BM_PushStdQueue(benchmark::State& state) {
+    // how many values to push to the queue
+    uint32_t num_values = state.range(0);
 
     for (auto _ : state) {
-        for (uint32_t i = 0; i < max; ++i) {
+        std::queue<uint32_t> queue;
+
+        for (uint32_t i = 0; i < num_values; ++i) {
             queue.push(i);
         }
+
+        benchmark::DoNotOptimize(queue);
     }
 }
 
 BENCHMARK(BM_PushStdQueue)->RangeMultiplier(10)->Range(100, 1000000);
+
+//------------------------------------------------------------------------------
 
 BENCHMARK_MAIN();
