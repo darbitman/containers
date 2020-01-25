b92a95bce2c2e7023207418617fc5a1c41e66c19
Dmitriy A <d.arbitman@gmail.com>
2020-01-25 08:26:34 -0800

    Update interface

--- src/InterfaceSingletonHolder.hpp
+++ src/InterfaceSingletonHolder.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cassert>
+#include <future>
 
 namespace ctr {
 
@@ -11,16 +12,13 @@ class InterfaceSingletonHolder {
 
     static void SetInstance(InterfaceType* p_interface) { p_interface_ = p_interface; }
 
-    template <typename ReturnType>
-    static ReturnType GetInstance() {
-        if constexpr (std::is_reference<ReturnType>::value) {
-            assert(p_interface_ != nullptr);
-            return *p_interface_;
-        }
-        if constexpr (std::is_pointer<ReturnType>::value) {
-            return p_interface_;
-        }
+    static InterfaceType& GetInstance() {
+        assert(p_interface_ != nullptr);
+
+        return *p_interface_;
     }
+
+    static InterfaceType* GetInstancePointer() { return p_interface_; }
 
   private:
     inline static InterfaceType* p_interface_ = nullptr;
