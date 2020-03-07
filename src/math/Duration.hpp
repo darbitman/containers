471713cd183751c4bcc1956fe4fd937e7647ef66
Dmitriy A <d.arbitman@gmail.com>
2020-03-07 11:25:32 -0800

    Adding math helpers

---
+++ src/math/Duration.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <array>
+
+class Duration {
+    static constexpr double kSecondsPerMillisecond = 0.001;
+    static constexpr double kSecondsPerMinute = 60.0;
+    static constexpr double kSecondsPerHour = 3600.0;
+
+  public:
+    enum class Unit { kHours, kMinutes, kSeconds, kMilliseconds };
+
+    constexpr Duration() noexcept : value_(0.0), unit_(Unit::kSeconds) {}
+
+    constexpr Duration(double value, Unit unit) noexcept : value_(value), unit_(unit) {}
+
+    ~Duration() noexcept = default;
+
+    constexpr double Get(Unit to_unit) const noexcept {
+        if (to_unit == unit_) {
+            return value_;
+        } else {
+            auto ratio = GetRatio(to_unit);
+            return value_ * ratio;
+        }
+    }
+
+    constexpr void Set(double value, Unit unit) noexcept {
+        value_ = value;
+        unit_ = unit;
+    }
+
+  private:
+    constexpr double GetConversionRatio(Unit unit) const {
+        for (auto& ratios : seconds_per_unit_) {
+            if (ratios.first == unit) {
+                return ratios.second;
+            }
+        }
+    }
+
+    constexpr double GetRatio(Unit to_unit) const {
+        return GetConversionRatio(unit_) / GetConversionRatio(to_unit);
+    }
+
+    double value_;
+    Unit unit_;
+
+    static constexpr std::array<std::pair<Duration::Unit, double>, 4> seconds_per_unit_{
+        {{Duration::Unit::kMilliseconds, Duration::kSecondsPerMillisecond},
+         {Duration::Unit::kSeconds, 1.0},
+         {Duration::Unit::kMinutes, Duration::kSecondsPerMinute},
+         {Duration::Unit::kHours, Duration::kSecondsPerHour}}};
+};
---
+++ src/math/Speed.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <array>
+
+class Speed {
+    static constexpr double kMetersPerSecondPerMPH = 3600.0 / (0.3048 * 5280);
+    static constexpr double kMetersPerSecondPerCmPerSecond = 0.01;
+
+  public:
+    enum class Unit { kMPH, kMetersPerSecond, kCmPerSecond };
+
+    constexpr Speed() noexcept : value_(0.0), unit_(Unit::kMetersPerSecond) {}
+
+    constexpr Speed(double value, Unit unit) noexcept : value_(value), unit_(unit) {}
+
+    ~Speed() noexcept = default;
+
+    constexpr double Get(Unit to_unit) const noexcept;
+
+  private:
+    double value_;
+    Unit unit_;
+
+    static constexpr std::array<std::pair<Speed::Unit, double>, 3> meters_per_second_per_unit_{
+        {{Speed::Unit::kMetersPerSecond, 1.0},
+         {Speed::Unit::kMPH, kMetersPerSecondPerMPH},
+         {Speed::Unit::kCmPerSecond, kMetersPerSecondPerCmPerSecond}}};
+};
