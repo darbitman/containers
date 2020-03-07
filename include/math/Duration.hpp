5f8589131a1f5503947a5bed98f4e468b953c908
Dmitriy A <d.arbitman@gmail.com>
2020-03-07 15:25:05 -0800

    Move definitions out of class

--- include/math/Duration.hpp
+++ include/math/Duration.hpp
@@ -12,33 +12,20 @@ class Duration {
   public:
     enum class Unit { kHours, kMinutes, kSeconds, kMilliseconds };
 
-    constexpr Duration() noexcept : value_(0.0), unit_(Unit::kSeconds) {}
+    constexpr Duration() noexcept;
 
-    constexpr Duration(double value, Unit unit) noexcept : value_(value), unit_(unit) {}
+    constexpr Duration(double value, Unit unit) noexcept;
 
     ~Duration() noexcept = default;
 
     constexpr double Get(Unit to_unit) const noexcept;
 
-    constexpr void Set(double value, Unit unit) noexcept {
-        value_ = value;
-        unit_ = unit;
-    }
+    constexpr void Set(double value, Unit unit) noexcept;
 
   private:
-    constexpr double GetConversionRatio(Unit unit) const {
-        for (auto& ratios : seconds_per_unit_) {
-            if (ratios.first == unit) {
-                return ratios.second;
-            }
-        }
+    constexpr double GetConversionRatio(Unit unit) const noexcept;
 
-        return 1.0;
-    }
-
-    constexpr double GetRatio(Unit to_unit) const {
-        return GetConversionRatio(unit_) / GetConversionRatio(to_unit);
-    }
+    constexpr double GetRatio(Unit to_unit) const noexcept;
 
     double value_;
     Unit unit_;
@@ -50,6 +37,11 @@ class Duration {
          {Duration::Unit::kHours, Duration::kSecondsPerHour}}};
 };
 
+constexpr Duration::Duration() noexcept : value_(0.0), unit_(Duration::Unit::kSeconds) {}
+
+constexpr Duration::Duration(double value, Duration::Unit unit) noexcept
+    : value_(value), unit_(unit) {}
+
 constexpr double Duration::Get(Unit to_unit) const noexcept {
     if (to_unit == unit_) {
         return value_;
@@ -57,6 +49,25 @@ constexpr double Duration::Get(Unit to_unit) const noexcept {
         auto ratio = GetRatio(to_unit);
         return value_ * ratio;
     }
+}
+
+constexpr void Duration::Set(double value, Duration::Unit unit) noexcept {
+    value_ = value;
+    unit_ = unit;
+}
+
+constexpr double Duration::GetConversionRatio(Duration::Unit unit) const noexcept {
+    for (auto& ratios : seconds_per_unit_) {
+        if (ratios.first == unit) {
+            return ratios.second;
+        }
+    }
+
+    return 1.0;
+}
+
+constexpr double Duration::GetRatio(Duration::Unit to_unit) const noexcept {
+    return GetConversionRatio(unit_) / GetConversionRatio(to_unit);
 }
 
 }  // namespace helpers::math
