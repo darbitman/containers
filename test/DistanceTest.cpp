dbc148c380f16b49c07b3da57ce23fbf35c59efa
Dmitriy A <d.arbitman@gmail.com>
2020-03-07 19:39:52 -0800

    Testing operator+

--- test/DistanceTest.cpp
+++ test/DistanceTest.cpp
@@ -12,5 +12,17 @@ TEST(DistanceTest, TestMeters) {
     EXPECT_EQ(miles, 1.0);
 }
 
+TEST(DistanceTest, TestAdditionOperator) {
+    constexpr Distance distance_a(100.0, Distance::Unit::kFoot);
+    constexpr Distance distance_b(100.0, Distance::Unit::kInch);
+
+    constexpr Distance distance_sum = distance_a + distance_b;
+    constexpr auto distance_sum_value = distance_sum.Get(Distance::Unit::kFoot);
+
+    // Floating point error might occur, so ensure value is between upper and lower bound
+    EXPECT_LT(distance_sum_value, 108.33334);
+    EXPECT_GT(distance_sum_value, 108.33333);
+}
+
 }  // namespace
 }  // namespace helpers::math
