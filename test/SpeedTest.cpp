04276d72d86d580a3d2be466f457ab1fa4979685
Dmitriy A <d.arbitman@gmail.com>
2020-03-07 19:18:50 -0800

    Testing fps

--- test/SpeedTest.cpp
+++ test/SpeedTest.cpp
@@ -10,6 +10,7 @@ TEST(SpeedTest, TestMetersPerSecond) {
 
     constexpr auto miles_per_hour = speed.Get(Speed::Unit::kMPH);
     constexpr auto kilometers_per_hour = speed.Get(Speed::Unit::kKPH);
+    constexpr auto feet_per_second = speed.Get(Speed::Unit::kFeetPerSecond);
 
     // Floating point error might occur, so ensure value is between upper and lower bound
     EXPECT_LT(miles_per_hour, 223.6937);
@@ -17,6 +18,9 @@ TEST(SpeedTest, TestMetersPerSecond) {
 
     EXPECT_LT(kilometers_per_hour, 360.0001);
     EXPECT_GT(kilometers_per_hour, 359.9999);
+
+    EXPECT_LT(feet_per_second, 328.0840);
+    EXPECT_GT(feet_per_second, 328.0839);
 }
 
 TEST(SpeedTest, TestMilesPerHour) {
@@ -24,6 +28,7 @@ TEST(SpeedTest, TestMilesPerHour) {
 
     constexpr auto meters_per_second = speed.Get(Speed::Unit::kMetersPerSecond);
     constexpr auto kilometers_per_hour = speed.Get(Speed::Unit::kKPH);
+    constexpr auto feet_per_second = speed.Get(Speed::Unit::kFeetPerSecond);
 
     // Floating point error might occur, so ensure value is between upper and lower bound
     EXPECT_LT(meters_per_second, 44.7045);
@@ -31,6 +36,9 @@ TEST(SpeedTest, TestMilesPerHour) {
 
     EXPECT_LT(kilometers_per_hour, 160.935);
     EXPECT_GT(kilometers_per_hour, 160.934);
+
+    EXPECT_LT(feet_per_second, 146.66667);
+    EXPECT_GT(feet_per_second, 146.66666);
 }
 
 TEST(SpeedTest, TestKilometersPerHour) {
@@ -38,6 +46,7 @@ TEST(SpeedTest, TestKilometersPerHour) {
 
     constexpr auto meters_per_second = speed.Get(Speed::Unit::kMetersPerSecond);
     constexpr auto miles_per_hour = speed.Get(Speed::Unit::kMPH);
+    constexpr auto feet_per_second = speed.Get(Speed::Unit::kFeetPerSecond);
 
     // Floating point error might occur, so ensure value is between upper and lower bound
     EXPECT_LT(meters_per_second, 27.77778);
@@ -45,6 +54,27 @@ TEST(SpeedTest, TestKilometersPerHour) {
 
     EXPECT_LT(miles_per_hour, 62.1372);
     EXPECT_GT(miles_per_hour, 62.1371);
+
+    EXPECT_LT(feet_per_second, 91.134445);
+    EXPECT_GT(feet_per_second, 91.134440);
+}
+
+TEST(SpeedTest, TestFeetPerSecond) {
+    constexpr Speed speed(100.0, Speed::Unit::kFeetPerSecond);
+
+    constexpr auto meters_per_second = speed.Get(Speed::Unit::kMetersPerSecond);
+    constexpr auto miles_per_hour = speed.Get(Speed::Unit::kMPH);
+    constexpr auto kilometers_per_hour = speed.Get(Speed::Unit::kKPH);
+
+    // Floating point error might occur, so ensure value is between upper and lower bound
+    EXPECT_LT(meters_per_second, 30.481);
+    EXPECT_GT(meters_per_second, 30.479);
+
+    EXPECT_LT(miles_per_hour, 68.18182);
+    EXPECT_GT(miles_per_hour, 68.18181);
+
+    EXPECT_LT(kilometers_per_hour, 109.72849);
+    EXPECT_GT(kilometers_per_hour, 109.72749);
 }
 
 }  // namespace
