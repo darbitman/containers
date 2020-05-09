9931049f48029bcfcc3806d802162c60c04a290d
Dmitriy A <d.arbitman@gmail.com>
2020-05-08 17:19:39 -0700

    Remove constexpr

--- test/DistanceTest.cpp
+++ test/DistanceTest.cpp
@@ -6,18 +6,18 @@ namespace helpers::math {
 namespace {
 
 TEST(DistanceTest, TestMeters) {
-    constexpr Distance distance(1609.344, Distance::Unit::kMeter);
+    Distance distance(1609.344, Distance::Unit::kMeter);
 
-    constexpr auto miles = distance.Get(Distance::Unit::kMile);
+    auto miles = distance.Get(Distance::Unit::kMile);
     EXPECT_EQ(miles, 1.0);
 }
 
 TEST(DistanceTest, TestAdditionOperator) {
-    constexpr Distance distance_a(100.0, Distance::Unit::kFoot);
-    constexpr Distance distance_b(100.0, Distance::Unit::kInch);
+    Distance distance_a(100.0, Distance::Unit::kFoot);
+    Distance distance_b(100.0, Distance::Unit::kInch);
 
-    constexpr Distance distance_sum = distance_a + distance_b;
-    constexpr auto distance_sum_value = distance_sum.Get(Distance::Unit::kFoot);
+    Distance distance_sum = distance_a + distance_b;
+    auto distance_sum_value = distance_sum.Get(Distance::Unit::kFoot);
 
     // Floating point error might occur, so ensure value is between upper and lower bound
     EXPECT_LT(distance_sum_value, 108.33334);
@@ -25,11 +25,11 @@ TEST(DistanceTest, TestAdditionOperator) {
 }
 
 TEST(DistanceTest, TestSubtractionOperator) {
-    constexpr Distance distance_a(100.0, Distance::Unit::kFoot);
-    constexpr Distance distance_b(100.0, Distance::Unit::kInch);
+    Distance distance_a(100.0, Distance::Unit::kFoot);
+    Distance distance_b(100.0, Distance::Unit::kInch);
 
-    constexpr Distance distance_difference = distance_a - distance_b;
-    constexpr auto distance_difference_value = distance_difference.Get(Distance::Unit::kFoot);
+    Distance distance_difference = distance_a - distance_b;
+    auto distance_difference_value = distance_difference.Get(Distance::Unit::kFoot);
 
     // Floating point error might occur, so ensure value is between upper and lower bound
     EXPECT_LT(distance_difference_value, 91.66667);
@@ -37,11 +37,11 @@ TEST(DistanceTest, TestSubtractionOperator) {
 }
 
 TEST(DistanceTest, TestSubtractionOperatorNegativeResult) {
-    constexpr Distance distance_a(100.0, Distance::Unit::kFoot);
-    constexpr Distance distance_b(100.0, Distance::Unit::kInch);
+    Distance distance_a(100.0, Distance::Unit::kFoot);
+    Distance distance_b(100.0, Distance::Unit::kInch);
 
-    constexpr Distance distance_difference = distance_b - distance_a;
-    constexpr auto distance_difference_value = distance_difference.Get(Distance::Unit::kFoot);
+    Distance distance_difference = distance_b - distance_a;
+    auto distance_difference_value = distance_difference.Get(Distance::Unit::kFoot);
 
     // Floating point error might occur, so ensure value is between upper and lower bound
     EXPECT_LT(distance_difference_value, -91.66666);
