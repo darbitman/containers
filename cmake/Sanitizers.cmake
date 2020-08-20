874533d421478ed3755443aca1681ec8b667f2f6
Dmitriy A <d.arbitman@gmail.com>
2020-08-19 19:40:16 -0700

    Adding sanitizers

---
+++ cmake/Sanitizers.cmake
@@ -0,0 +1,38 @@
+function(enable_compiler_sanitizers target_name)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(SANITIZERS "")
+
+    option(ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" FALSE)
+    if(ENABLE_SANITIZER_ADDRESS)
+      list(APPEND SANITIZERS "address")
+    endif()
+
+    option(ENABLE_SANITIZER_LEAK "Enable leak sanitizer" FALSE)
+    if(ENABLE_SANITIZER_LEAK)
+      list(APPEND SANITIZERS "leak")
+    endif()
+
+    option(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR "Enable undefined behavior sanitizer" FALSE)
+    if(ENABLE_SANITIZER_UNDEFINED_BEHAVIOR)
+      list(APPEND SANITIZERS "undefined")
+    endif()
+
+    option(ENABLE_SANITIZER_THREAD "Enable memory sanitizer" FALSE)
+    if(ENABLE_SANITIZER_THREAD )
+      if("address" IN_LIST SANITIZERS OR "leak" IN_LIST SANITIZERS)
+        message(WARNING "Memory sanitizer does not work with address or leak sanitizers enabled")
+      else()
+        list(APPEND SANITIZERS "memory")
+      endif()
+    endif()
+
+    list(JOIN SANITIZERS "," LIST_OF_SANITIZERS)
+  endif()
+
+  if(LIST_OF_SANITIZERS)
+    if(NOT "${LIST_OF_SANITIZERS}" STREQUAL "")
+      target_compile_options(${target_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+      target_link_libraries(${target_name} INTERFACE -fsanitize=${LIST_OF_SANITIZERS})
+    endif()
+  endif()
+endfunction()
